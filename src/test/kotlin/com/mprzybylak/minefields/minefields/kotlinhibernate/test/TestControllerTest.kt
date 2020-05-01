0a0be4fafd43c82c2a5a4ee6447d48118ac5e7e3
Michał Przybylak <michal.przybylak@gmail.com>
2020-05-01 22:29:12 +0200

    optimize imports

--- src/test/kotlin/com/mprzybylak/minefields/minefields/kotlinhibernate/test/TestControllerTest.kt
+++ src/test/kotlin/com/mprzybylak/minefields/minefields/kotlinhibernate/test/TestControllerTest.kt
@@ -2,16 +2,13 @@ package com.mprzybylak.minefields.minefields.kotlinhibernate.test
 
 import com.mprzybylak.minefields.minefields.kotlinhibernate.KotlinHibernateApplication
 import org.junit.Test
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.annotation.DirtiesContext.ClassMode
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.transaction.TransactionSystemException
